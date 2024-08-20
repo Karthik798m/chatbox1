6ff368102459c2925ab81e340c5932eb5a099e60
Karthik798m <karthikmanoj798m@gmail.com>
2024-08-20 14:26:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+
+function fun(){
+    if(document.querySelector("input").value==''){
+        document.querySelector(".chatlist").appendChild()
+    }
+    else{
+        const a=document.createElement("li")
+        document.querySelector(".chatlist").appendChild(a)
+        a.classList.add("chat")
+
+
+        const b=document.createElement("div")
+        document.querySelector(".chat").appendChild(b)
+        b.classList.add("my")
+
+
+
+
+        if(document.querySelector(".chat").firstChild.className=='my'){
+        b.innerHTML+='<p class="h">you</p>' +document.querySelector("input").value 
+        }
+
+        else if(document.querySelector(".chat").firstChild.className=='client'){
+        b.innerHTML+='<p class="h">other</p>' +document.querySelector("input").value 
+        }
+        document.querySelector("input").value="";
+        document.querySelector(".chatlist").lastChild.scrollIntoView();
+        
+        
+    }
+}
+
+document.querySelector(".send").addEventListener("click",fun)
+
